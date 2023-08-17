b533c0c396fed33c1b69d463a7c1a97cc4d312c0
giulianac-git <79769030+giulianac-git@users.noreply.github.com>
2023-08-17 01:50:32 +0000

    [Datastructures and Endpoints Added] - src/datastructures.py: Added functions to view single member, delete member and add member. - src/app.py: Added the GET, POST and DELETE Endpoints.

--- src/app.py
+++ src/app.py
@@ -30,13 +30,46 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({"msg": "You must send information inside the body"}), 400 
+    if "first_name" not in body:
+        return jsonify({"msg": "You must send the first name"}), 400
+    if "age" not in body:
+        return jsonify({"msg": "You must send the age"}), 400
+    if "lucky_numbers" not in body:
+        return jsonify({"msg": "You must send an array with the lucky numbers"}), 400
+    id = jackson_family._generateId()
+    if "id" in body:
+        id = body["id"]
+
+    member = {
+        "id": id,
+        "first_name": body["first_name"],
+        "last_name": jackson_family.last_name,
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
     }
+    jackson_family.add_member(member)
+    return jsonify(member), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_single_member(id):
+    single_member = jackson_family.get_member(id)
+    if single_member is None:
+        raise APIException(f"The user id {id} doesn't exist", status_code=400)
+    return jsonify(single_member), 200
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_single_member(id):
+    if id is None:
+        raise APIException(f"The user id {id} doesn't exist", status_code=400)
+    jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
